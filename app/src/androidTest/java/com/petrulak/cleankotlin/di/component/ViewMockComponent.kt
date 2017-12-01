23fed4614e266068e85ce765a6861fe823d3b884
Martin Petrulak <mato.petrulak@gmail.com>
2017-12-01 19:35:39 +0100

    Update tests

--- app/src/androidTest/java/com/petrulak/cleankotlin/di/component/ViewMockComponent.kt
+++ app/src/androidTest/java/com/petrulak/cleankotlin/di/component/ViewMockComponent.kt
@@ -3,6 +3,7 @@ package com.petrulak.cleankotlin.di.component
 import com.petrulak.cleankotlin.di.module.PresenterMockModule
 import com.petrulak.cleankotlin.di.scope.ViewScope
 import com.petrulak.cleankotlin.ui.example2.Example2ActivityTest
+import com.petrulak.cleankotlin.ui.example2.Example2PresenterTest
 import dagger.Component
 
 @ViewScope
@@ -12,4 +13,5 @@ import dagger.Component
 interface ViewMockComponent {
 
     fun inject(item: Example2ActivityTest)
+    fun inject(item: Example2PresenterTest)
 }
